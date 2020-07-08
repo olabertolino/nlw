31d34663cd115366eb65a5d4393347c7154a55e4
André Bertolino <olabertolino@gmail.com>
2020-07-08 20:02:15 -0300

    criando tabelas database

--- src/database/db.js
+++ src/database/db.js
@@ -1,3 +1,34 @@
 const sqlite3 = require("sqlite3").verbose()
 
 const db = new sqlite3.Database("./src/database/database.db")
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS places (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            name TEXT,
+            address TEXT,
+            address2 TEXT,
+            state  TEXT,
+            city TEXT,
+            items TEXT
+        );
+    `)
+
+
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+    db.run(query, [])
+})
+
+// assisti até 30 minutos da última aula
